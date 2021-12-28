503b7ff9e85ecddd1ee2e3252e37febdaea86038
denver-01 <96603479+denver-01@users.noreply.github.com>
2021-12-28 11:54:14 +0800

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    var typed = new Typed('#typing', {
+        strings: [
+            "Low-End",
+            "Mid-End",
+            "High-End"
+        ],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+      });
+});
